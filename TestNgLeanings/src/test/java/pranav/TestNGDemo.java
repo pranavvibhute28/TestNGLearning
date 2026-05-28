acf41873e156f9c338790424aac3bbd0b49f6849
PranavVibhute <pranavibhute4141@gmail.com>
2026-05-28 14:19:01 +0530

    adding TestNG Demo project

---
+++ TestNgLeanings/src/test/java/pranav/TestNGDemo.java
@@ -0,0 +1,13 @@
+package pranav;
+
+import org.testng.annotations.Test;
+
+public class TestNGDemo {
+
+	@Test
+	public void TestCase01() {
+		System.out.println("This is test case 1");
+	}
+	
+	
+}
